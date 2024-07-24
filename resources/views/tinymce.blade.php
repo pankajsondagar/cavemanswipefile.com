3ae22d0d1577dd91a682a1216e549e1b23aa19ea
Pankaj Sondagar <pankaj@krishaweb.com>
2024-07-24 20:44:40 +0530

    Fixed Ckeditor issue

--- resources/views/tinymce.blade.php
+++ resources/views/tinymce.blade.php
@@ -1,24 +1,6 @@
 @section('script')
-    <script src="https://cdn.ckeditor.com/4.10.1/standard/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
     <script>
-        //deal with copying the ckeditor text into the actual textarea
-        CKEDITOR.on('instanceReady', function () {
-            $.each(CKEDITOR.instances, function (instance) {
-                CKEDITOR.instances[instance].document.on("keyup", CK_jQ);
-                CKEDITOR.instances[instance].document.on("paste", CK_jQ);
-                CKEDITOR.instances[instance].document.on("keypress", CK_jQ);
-                CKEDITOR.instances[instance].document.on("blur", CK_jQ);
-                CKEDITOR.instances[instance].document.on("change", CK_jQ);
-            });
-        });
-
-        function CK_jQ() {
-            for (instance in CKEDITOR.instances) {
-                CKEDITOR.instances[instance].updateElement();
-            }
-        }
-
-        CKEDITOR.replace('editor1', {height: 450});
-
+        ClassicEditor.create(document.querySelector('#editor1'));
     </script>
 @endsection
